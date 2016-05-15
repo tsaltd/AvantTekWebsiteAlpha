8985e70d4ea068753882d644940381b67c1750f4
tsaltd <wp710012@gmail.com>
2016-05-15 15:13:27 -0400

    please work

---
+++ Workspace/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Workspace.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AvantTekAlpha()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Workspace/wwwroot/js/avant-controller-email.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    angular.module('app-email', [])
+        .controller('emailController', [
+            '$scope', function($scope) {
+                $scope.email = {
+                    text: 'me@example.com',
+                    wtf: 'fixed !'
+            };
+                $scope.validateEmail = function ()
+                {
+                    $scope.wtf ='worked !';
+
+                };
+    }]);
+})();
