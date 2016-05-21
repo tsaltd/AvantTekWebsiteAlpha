acb09b8c734bddfcd5f6367e33ca6435482bbd8f
tsaltd <wp710012@gmail.com>
2016-05-20 23:24:01 -0400

    DidntLearnToClick

--- Workspace/Controllers/HomeController.cs
+++ Workspace/Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace Workspace.Controllers
         {
             return View();
         }
+        public IActionResult LearnToClick()
+        {
+            return View();
+        }
         public IActionResult AvantTekAlpha()
         {
             return View();
