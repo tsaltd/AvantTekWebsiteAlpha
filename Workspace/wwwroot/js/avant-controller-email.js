72542c08b8263d6f19a3b6ef3231daf966cb89a3
tsaltd <wp710012@gmail.com>
2016-05-31 22:11:26 -0400

    BuildNewVS2013Component

--- Workspace/wwwroot/js/avant-controller-email.js
+++ Workspace/wwwroot/js/avant-controller-email.js
@@ -1,24 +1,51 @@
 ﻿(function () {
     "use strict";
-        angular.module("app-email")
-            .controller("emailController", emailController);
-        function emailController() {
+    angular.module("app-email")
+        .controller("emailController", emailController);
 
-            var vm = this;
+    function emailController($http) {
 
-            vm.wtf = "hello";
+        var vm = this;
 
-            vm.email = {
-                text: "steve.mintz@gmail.com"
-            //    sender:   document.getElementById("ValidEmail")
-            }
-            vm.validateEmail = function() {
-                alert("wtf");
-            }
-            vm.SendGridAlphaInterested = function() {
-                alert("Interested");
-            }
+        vm.wtf = "hello";
 
+
+        vm.alphaEmail = {
+            From: "stevem@avanttek.com",
+            Subject: "AlphaInterested",
+            Text: "MessageText",
+            To: "alphauser@avanttek.com"
         }
 
+
+        vm.email = {
+            text: "steve.mintz@gmail.com"
+            //    sender:   document.getElementById("ValidEmail")
+        }
+        vm.validateEmail = function() {
+            alert("wtf");
+        }
+        vm.SendGridAlphaInterested = function () {
+            vm.isbusy = true;
+         //   alert("Interested");
+            $http.post("/api/Message", vm.alphaEmail)
+                .then(function () {
+                    //success
+                    alert("yes");
+
+                },
+                    function () {
+                        //failure
+                        alert("no");
+                    })
+                .finally(function () {
+                    vm.busy = false;
+                });
+    };
+
+};
+
 })();
+
+
+
