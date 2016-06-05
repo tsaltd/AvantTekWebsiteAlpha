57b24a96e51cff9945853c894edaabe657a8616f
tsaltd <wp710012@gmail.com>
2016-06-05 09:59:46 -0400

    Revert "didnt work"
    This reverts commit 206379cbedf4077099b0817861458d1caad53e47.

--- Workspace/wwwroot/js/avant-controller-email.js
+++ Workspace/wwwroot/js/avant-controller-email.js
@@ -3,21 +3,19 @@
     angular.module("app-email")
         .controller("emailController", emailController);
 
-    function emailController($scope, $http) {
+    function emailController($http) {
 
         var vm = this;
 
         vm.wtf = "hello";
-        vm.email.text = "sdm@me.com";
 
 
         vm.alphaEmail = {
-            //    From: "stevem@avanttek.com",
-            //  Subject: "AlphaInterested",
-            //  Text: "MessageText"
-            From: "steve.mintz@gmail.com"
-        //   To: "alphauser@avanttek.com"
-    }
+            From: "stevem@avanttek.com",
+            Subject: "AlphaInterested",
+            Text: "MessageText",
+            To: "alphauser@avanttek.com"
+        }
 
 
         vm.email = {
@@ -28,12 +26,9 @@
             alert("wtf");
         }
         vm.SendGridAlphaInterested = function () {
-          //  vm.isbusy = true;
+            vm.isbusy = true;
          //   alert("Interested");
-            //$http.post("/api/Message", vm.alphaEmail)
-            var tester = "http://www.cuisinecuration.com/api/sendgrid/send?who=" + vm.alphaEmail.From;
-            alert(tester);
-            $http.post(tester)
+            $http.post("/api/Message", vm.alphaEmail)
                 .then(function () {
                     //success
                     alert("yes");
@@ -41,7 +36,7 @@
                 },
                     function () {
                         //failure
-                        alert(vm.email.text);
+                        alert("no");
                     })
                 .finally(function () {
                     vm.busy = false;
