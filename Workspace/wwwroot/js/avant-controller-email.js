206379cbedf4077099b0817861458d1caad53e47
tsaltd <wp710012@gmail.com>
2016-06-05 09:59:25 -0400

    didnt work

--- Workspace/wwwroot/js/avant-controller-email.js
+++ Workspace/wwwroot/js/avant-controller-email.js
@@ -3,19 +3,21 @@
     angular.module("app-email")
         .controller("emailController", emailController);
 
-    function emailController($http) {
+    function emailController($scope, $http) {
 
         var vm = this;
 
         vm.wtf = "hello";
+        vm.email.text = "sdm@me.com";
 
 
         vm.alphaEmail = {
-            From: "stevem@avanttek.com",
-            Subject: "AlphaInterested",
-            Text: "MessageText",
-            To: "alphauser@avanttek.com"
-        }
+            //    From: "stevem@avanttek.com",
+            //  Subject: "AlphaInterested",
+            //  Text: "MessageText"
+            From: "steve.mintz@gmail.com"
+        //   To: "alphauser@avanttek.com"
+    }
 
 
         vm.email = {
@@ -26,9 +28,12 @@
             alert("wtf");
         }
         vm.SendGridAlphaInterested = function () {
-            vm.isbusy = true;
+          //  vm.isbusy = true;
          //   alert("Interested");
-            $http.post("/api/Message", vm.alphaEmail)
+            //$http.post("/api/Message", vm.alphaEmail)
+            var tester = "http://www.cuisinecuration.com/api/sendgrid/send?who=" + vm.alphaEmail.From;
+            alert(tester);
+            $http.post(tester)
                 .then(function () {
                     //success
                     alert("yes");
@@ -36,7 +41,7 @@
                 },
                     function () {
                         //failure
-                        alert("no");
+                        alert(vm.email.text);
                     })
                 .finally(function () {
                     vm.busy = false;
